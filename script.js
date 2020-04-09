acf4c29db34894654d275198af4479456ea3dbf6
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-04-08 21:21:41 -0600

    2nd push

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var key= '0IA37F9JE1OFIMZM'
 var company = "GSPC"
-//wanted companies DJI, NDAQ, GSPC
+// wanted companies DJI, NDAQ, GSPC
 var queryURL = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${company}&apikey=${key}`
+// https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=BA&apikey=demo
 
 $.ajax({
     url: queryURL,
